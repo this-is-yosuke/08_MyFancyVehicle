5751fafff454455c218936eda52128b46a0499ac
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-27 15:05:05 -0600

    Updated Motorbike.ts
    Added wheelie method

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -64,6 +64,9 @@ class Motorbike extends Vehicle{
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+  wheelie(): void {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+  };
 
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
