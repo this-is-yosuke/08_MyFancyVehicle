433819584625ba0fd91a797fae0c10e3a6032dcb
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-27 14:55:53 -0600

    Updated Motorbike.ts
    added the constructor for Motorbike

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -8,26 +8,59 @@ class Motorbike extends Vehicle{
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
   vin: string;
-  color: String;
+  color: string;
   make: string;
   model: string;
   year: number;
   weight: number;
   topSpeed: number;
   wheels: Wheel[];
-  // Motorbikes either have two-stroke or four-stroke engines
-  two-strokeEngine: boolean;
-  // motorbikes either have one or two shock absorbers
+  // Motorbikes either have one or two shock absorbers
   singleShockAbsorber: boolean;
-  // purposes may include: street, touring, off-road, racing, or trail
+  // Motorbikes either have two-stroke or four-stroke engines
+  twoStrokeEngine: boolean;
+  // Purposes may include: street, touring, off-road, racing, or trail
   purpose: string;
   // There are 26 types, including Electric, Scambler, and Sidecar
   type: string;
-
+  
   // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Motorbike class
+  // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    singleShockAbsorber: boolean,
+    twoStrokeEngine: boolean,
+    purpose: string,
+    type: string,
+  ) {
+    super();
+    // Initialize properties
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    if(wheels.length !== 2){
+      this.wheels = [new Wheel(), new Wheel]
+    } else {
+      this.wheels = wheels;
+    }
+    this.singleShockAbsorber = singleShockAbsorber;
+    this.twoStrokeEngine = twoStrokeEngine;
+    this.purpose = purpose;
+    this.type = type;
+  }
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
