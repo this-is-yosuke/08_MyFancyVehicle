92ccd6a39f47508d4ef38d99ac32901dc5ed9289
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-28 12:42:30 -0600

    Updated truck.ts
    Added properties to Truck

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -10,6 +10,15 @@ class Truck {
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
 
   // TODO: Create a constructor that accepts the properties of the Truck class
     // TODO: The constructor should call the constructor of the parent class, Vehicle
