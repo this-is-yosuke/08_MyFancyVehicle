e6fb99aeb17a6518d2af4ea9c9c4dfb94f889be4
this-is-yosuke <yosuke.kibe@gmail.com>
2024-10-28 13:12:24 -0600

    Updated truck.ts
    Added the constructor and it now implements AbleToTow interface.

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -6,7 +6,7 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck {
+class Truck extends Vehicle implements AbleToTow{
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
@@ -19,12 +19,38 @@ class Truck {
   topSpeed: number;
   wheels: Wheel[];
   towingCapacity: number;
-
+  
   // TODO: Create a constructor that accepts the properties of the Truck class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Truck class
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Truck class
+  // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+  constructor (
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number,
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    if(wheels.length !== 4){
+      this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    };
+    this.towingCapacity = towingCapacity;
+  }
+  
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
